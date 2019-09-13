855a6584dddf860bfb5565e7e48abd2e102477cb
Gauravdeep Gosal <gdeep141@gmail.com>
2019-09-13 08:52:13 +0100

    added STATIC_ROOT

--- cheesecake_club/settings.py
+++ cheesecake_club/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Full path to directory where django should store uploaded files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
